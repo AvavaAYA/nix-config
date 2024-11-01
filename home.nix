f6d441fdc0ab34ef7b95d3333090593d250bc959
avavaaya <nana7777777mi@gmail.com>
2024-11-01 20:39:07 +0800

    updating lianpwn

--- home.nix
+++ home.nix
@@ -111,10 +111,10 @@ let
         # lianpwn
         buildPythonPackage rec {
           pname = "lianpwn";
-          version = "1.0.5";
+          version = "1.0.6";
           src = fetchPypi {
             inherit pname version;
-            sha256 = "sha256-o87+xFX0pgAMsNqrlNG7TejUAXNKvuy8ivpu6pjnEkA=";
+            sha256 = "sha256-TBqnhrMU7wHRfeJCH0r+IuGvS8GOgqKafdwpBMdXEDA=";
           };
           propagatedBuildInputs = [ ];
           doCheck = false;
